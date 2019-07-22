68a1665f880977b0a1bae6144a43563348047361
yaala21-meet <yaala21@meet.mit.edu>
2019-07-22 04:01:03 -0400

    Stopping here

--- lab_1.py
+++ lab_1.py
@@ -1,10 +1,42 @@
 import turtle
 
-# m
+# first ^
 turtle.penup()
-turtle.goto(-100,0)
+turtle.goto(-175,100)
 turtle.pendown()
-turtle.left(90)
+turtle.left(45)
 turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+# second ^
+turtle.penup()
+turtle.left(90)
+turtle.goto(175,100)
+turtle.pendown()
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+#  ( 
+turtle.penup()
+turtle.goto(-150,0)
+turtle.pendown()
+turtle.forward(100)
+turtle.left(22.5)
+turtle.forward(75)
+turtle.left(22.5)
+turtle.forward(150)
+turtle.left(22.5)
+turtle.forward(75)
+turtle.left(22.5)
+turtle.forward(100)
+
+# |
+turtle.penup()
+turtle.goto(-150,0)
+turtle.pendown()
+turtle.right(45)
+turtle.forward(450)
 
 turtle.mainloop()

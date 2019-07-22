cba647bbda4c2cb1424aa73f586d485f57f6c3d7
yaala21-meet <yaala21@meet.mit.edu>
2019-07-22 05:53:49 -0400

    Stopping here

--- lab_1.py
+++ lab_1.py
@@ -1,42 +1,62 @@
 import turtle
-
-# first ^
+# space: 50 width: 100
+# y -200 ~ -100
 turtle.penup()
-turtle.goto(-175,100)
+turtle.goto(-200,200)
+turtle.right(90)
 turtle.pendown()
-turtle.left(45)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.right(180)
+turtle.forward(300)
+# a -50 ~ 50
+turtle.penup()
+turtle.goto (-50,200)
+turtle.pendown()
+turtle.forward(80)
+turtle.left(90)
 turtle.forward(100)
 turtle.right(90)
+turtle.forward(20)
+turtle.left(180)
 turtle.forward(100)
-
-# second ^
+turtle.left(90)
+turtle.forward(100)
+# a 100 ~ 200
 turtle.penup()
 turtle.left(90)
-turtle.goto(175,100)
+turtle.goto (100,200)
 turtle.pendown()
+turtle.forward(80)
+turtle.left(90)
 turtle.forward(100)
 turtle.right(90)
+turtle.forward(20)
+turtle.left(180)
 turtle.forward(100)
-
-#  ( 
+turtle.left(90)
+turtle.forward(100)
+# l 250
 turtle.penup()
-turtle.goto(-150,0)
+turtle.left(90)
+turtle.goto(250,250)
 turtle.pendown()
-turtle.forward(100)
-turtle.left(22.5)
-turtle.forward(75)
-turtle.left(22.5)
 turtle.forward(150)
-turtle.left(22.5)
-turtle.forward(75)
-turtle.left(22.5)
-turtle.forward(100)
-
-# |
+# a 300 ~ 400
 turtle.penup()
-turtle.goto(-150,0)
+turtle.goto (300,200)
 turtle.pendown()
-turtle.right(45)
-turtle.forward(450)
+turtle.forward(80)
+turtle.left(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(20)
+turtle.left(180)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
 
 turtle.mainloop()
